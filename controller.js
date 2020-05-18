311c8aed44fdad0b24398f6431081456dc63ba0b
pandubidubidam <pandubidubidam@gmail.com>
2020-05-18 18:36:16 +0700

    menambahkan code controller.js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connectin = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi REST API ku berjalan")
+    response.ok("Aplikasi REST API ku berjalan",res)
 };
